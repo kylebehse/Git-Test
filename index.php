8e158bd758d78054553539e19241dfdd956954dd
Kyle Behse <kyle.behse@gmail.com>
2013-04-15 21:56:43 -0400

    Revert "Revert "Revert "Revert "commit with conflict""""
    This reverts commit 4a2055a7a571d1bf68147b4c8012e7400ea85dbe.

--- index.php
+++ index.php
@@ -5,7 +5,13 @@
 	</head>
 	
 	<body>
+<<<<<<< HEAD
 		<p>Some content.</p>
 		<p>Branch two.</p>
+=======
+		<p>Some content. Master.</p>
+		<p>Some content. Branch two.</p>
+		<p>Some content. Branch three.</p>
+>>>>>>> branch-three
 	</body>
 </html>
