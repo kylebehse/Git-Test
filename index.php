870c7a075416bcb6371c2753bc8ef8048ff40cd5
Kyle Behse <kyle.behse@gmail.com>
2013-09-27 23:57:26 -0400

    dded a line

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
 		<p>Some content. Master.</p>
 		<p>Some content. Branch two.</p>
 		<p>Some content. Branch three.</p>
+		<p>This is a conflict.</p>
+		<p>Another line of text.</p>
 		<p>Try another commit.</p>
 	</body>
 </html>
